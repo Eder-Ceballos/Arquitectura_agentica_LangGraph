b7cce5a606432d156d8bfd3e8f170ebf63fceb0a
Eder-Ceballos <ederceballos874@gmail.com>
2026-04-12 16:32:04 -0500

    feat/modo producction

--- backend/agents/graph.py
+++ backend/agents/graph.py
@@ -25,32 +25,20 @@ def create_graph():
     workflow = StateGraph(AgentState)
 
     # 1. Añadir los agentes (Nodos)
-    #workflow.add_node("profile_analyzer", profile_node)
-    workflow.add_node("profile_analyzer", mock_profile_node)
+    workflow.add_node("profile_analyzer", profile_node)
+    #workflow.add_node("profile_analyzer", mock_profile_node)
     
     # Envolvemos el validador para decirle que evalúe el "perfil"
     def validador_perfil(state: AgentState):
         return universal_validator_node(state, target="profile")
         
-    workflow.add_node("validator", mock_validator_node)
+    workflow.add_node("validator", universal_validator_node)
 
     # 2. Definir el flujo inicial: Inicio -> Perfil -> Validador
     workflow.set_entry_point("profile_analyzer")
     workflow.add_edge("profile_analyzer", "validator")
 
-    # 3. La Bifurcación (Conditional Edge)
-    # Aquí el grafo "pregunta" qué camino tomar basándose en el state["es_valido"]
-    workflow.add_conditional_edges(
-        "validator",
-        should_continue,
-        {
-            "buscar_vacantes": "vacancy_scraper", # Camino A: Todo bien, busca empleo
-            "detener": END                        # Camino B: Faltan datos, aborta misión
-        }
-    )
 
-    # 4. Si llegó a buscar vacantes, ese es el final del proceso (por ahora)
-    workflow.add_edge("vacancy_scraper", END)
 
     return workflow.compile()
 
